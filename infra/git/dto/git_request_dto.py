68106cb88eef0f6440f4e7b90d1d199e321a9a47
Vagner Gross Klaus <vagnergrossklaus@gmail.com>
2024-01-06 17:22:51 -0300

    feat: Criar implementacao para github #11 - Consultar merge request - Consultar dados do merge request
    [Issue](https://github.com/automatic-code-review/automatic-code-review-processor/issues/11)

---
+++ infra/git/dto/git_request_dto.py
@@ -0,0 +1,9 @@
+class GitRequestDTO:
+    def __init__(self):
+        self.title = ""
+        self.assignee = ""
+        self.source_branch = ""
+        self.target_branch = ""
+        self.author['username'] = ""
+        self.web_url = ""
+        self.created_at = ""
--- infra/git/github/github_wrapper.py
+++ infra/git/github/github_wrapper.py
@@ -1,3 +1,12 @@
+import os
+import shutil
+import subprocess
+from datetime import timezone
+
+from github import Github
+from github import Auth
+
+from infra.git.dto.git_request_dto import GitRequestDTO
 from infra.git.git_wrapper import GitWrapper
 
 
@@ -5,22 +14,43 @@ from infra.git.git_wrapper import GitWrapper
 class GitHubWrapper(GitWrapper):
 
     def __init__(self, git_url, git_token):
-        pass
+        self.git_url = git_url
+        self.git_token = git_token
+
+        auth = Auth.Token("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")
+        self.github_api = Github(auth=auth)
 
     def get_http_url_by_project_id(self, id_project):
-        pass
+        return self.github_api.get_repo(id_project).clone_url
 
     def get_id_project_source_by_id_project_target(self, id_project_target, id_merge_request):
-        pass
+        return self.github_api.get_repo(id_project_target).get_pull(int(id_merge_request)).base.repo.full_name
 
     def get_changes_by_merge(self, id_merge_request, id_project):
-        pass
+        changes = []
+        for file in self.github_api.get_repo(id_project).get_pull(int(id_merge_request)).get_files().get_page(0):
+            change = {'old_path': file.filename, 'new_path': file.filename}
+            changes.append(change)
+        return changes
 
     def get_merge_request(self, id_merge_request, id_project):
-        pass
+        pull_request = self.github_api.get_repo(id_project).get_pull(int(id_merge_request))
+        dto = GitRequestDTO
+        dto.title = pull_request.title
+        dto.assignee = pull_request.assignee
+        dto.source_branch = pull_request.head.ref
+        dto.target_branch = pull_request.base.ref
+        dto.author = {'username': pull_request.user.login}
+        dto.web_url = pull_request.url
+        dto.created_at = pull_request.created_at.strftime('%Y-%m-%dT%H:%M:%S')
+        return dto
 
     def clone_repo(self, url, branch, path):
-        pass
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        os.makedirs(path)
+        command = ["git", "clone", "-b", branch, url, path]
+        subprocess.run(command)
 
     def get_threads_by_merge_request(self, id_project, id_merge_request):
         pass
